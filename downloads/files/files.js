c4f09fbababc903070003e2a77f68789adda7375
ITCOM <159421917+Sysyz-itcom@users.noreply.github.com>
2025-05-18 00:59:57 +0800

    Update files.js

--- downloads/files/files.js
+++ downloads/files/files.js
@@ -1,1 +1,41 @@
-
+const allFiles = [
+    {
+      id: 1,
+      name: '邵阳市第一中学校园地图.apk',
+      type: 'Android应用',
+      size: '2.91 MB',
+      modified: '2025-02-05',
+      icon: 'fa-file-code',
+      color: 'text-yellow-600',
+      downloadUrls: {
+        default: 'https://share.feijipan.com/s/u8FGzVTl',
+        alternate: 'https://share.feijipan.com/s/u8FGzVTl'
+      }
+    },
+    {
+      id: 2,
+      name: 'WPS2019集成序列号安装包（事业单位）.exe',
+      type: 'Windows程序',
+      size: '171.97 MB',
+      modified: '2025-04-15',
+      icon: 'fa-file-code',
+      color: 'text-yellow-600',
+      downloadUrls: {
+        default: 'https://share.feijipan.com/s/zG0BtVSB',
+        alternate: 'https://share.feijipan.com/s/zG0BtVSB'
+      }
+    },
+    {
+      id: 3,
+      name: '非希沃一体机希沃白板识别增强插件.exe',
+      type: 'Windows程序',
+      size: '58.2 MB',
+      modified: '2025-03-15',
+      icon: 'fa-file-code',
+      color: 'text-yellow-600',
+      downloadUrls: {
+        default: 'https://share.feijipan.com/s/uJC73Wrz',
+        alternate: 'https://share.feijipan.com/s/uJC73Wrz'
+      }
+    }
+  ];    
