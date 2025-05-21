0e2aee88b73a65fab0a8f4d507722f84bbbaeca6
ITCOM <159421917+Sysyz-itcom@users.noreply.github.com>
2025-05-21 16:09:24 +0800

    Create config.js

---
+++ downloads/files/config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+            darkMode: 'class',
+            theme: {
+                extend: {
+                    colors: {
+                        primary: '#0078d4',
+                        secondary: '#60a5fa',
+                        neutral: '#f3f4f6',
+                        dark: '#1f2937',
+                    },
+                    fontFamily: {
+                        inter: ['Inter', 'sans-serif'],
+                    },
+                },
+            }
+        }
