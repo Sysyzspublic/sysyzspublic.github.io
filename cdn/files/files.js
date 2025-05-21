74dc707b414f3bf2bf70152f4f6492d6ddbee3cc
ITCOM <159421917+Sysyz-itcom@users.noreply.github.com>
2025-05-21 18:40:53 +0800

    Create files.js

---
+++ cdn/files/files.js
@@ -0,0 +1,38 @@
+const cdnFiles = [
+    {
+        name: "Tailwind CSS",
+        version: "3.3.3",
+        type: "CSS",
+        url: "https://sysyzspublic.github.io/cdn/files/cdn/tailwind.min.css"
+    },
+    {
+        name: "Jquery",
+        version: "3.6.4",
+        type: "JavaScript",
+        url: "https://sysyzspublic.github.io/cdn/files/cdn/jquery.min.js"
+    },
+    {
+        name: "BootStrap",
+        version: "5.3.0",
+        type: "CSS",
+        url: "https://sysyzspublic.github.io/cdn/files/cdn/bootstrap.min.css"
+    },
+    {
+        name: "React-dom",
+        version: "18.2.0",
+        type: "JavaScript",
+        url: "https://sysyzspublic.github.io/cdn/files/cdn/react-dom.production.min.js"
+    },
+    {
+        name: "axios",
+        version: "1.4.0",
+        type: "JavaScript",
+        url: "https://sysyzspublic.github.io/cdn/files/cdn/axios.min.js"
+    },
+    {
+        name: "chart.js",
+        version: "4.4.8",
+        type: "JavaScript",
+        url: "https://sysyzspublic.github.io/cdn/files/cdn/chart.umd.min.js"
+    }
+];
